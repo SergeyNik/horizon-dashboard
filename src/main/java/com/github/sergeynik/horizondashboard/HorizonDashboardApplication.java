9c748a512ce22a1d87280e3c60d7ca41156274c3
snikolchikov <nikolchikov@gmail.com>
2025-02-20 18:45:43 +0300

    app init

---
+++ src/main/java/com/github/sergeynik/horizondashboard/HorizonDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.github.sergeynik.horizondashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorizonDashboardApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/github/sergeynik/horizondashboard/HorizonDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sergeynik.horizondashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizonDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
